160ce1998a176e776925a3d836328f4e0f211ca6
Joaquim Rego <kimile@gmail.com>
2012-04-23 00:29:04 +0100

    Fixing problems with previous merge! Damn you git client

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,10 +2,6 @@ package controllers;
 
 import com.avaje.ebean.Ebean;
 import com.avaje.ebean.InvalidValue;
-<<<<<<< HEAD
-
-=======
->>>>>>> Merge registerAction() including Ebean property validations
 import models.*;
 import play.data.Form;
 import play.data.validation.ValidationError;
@@ -14,12 +10,9 @@ import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.*;
 
-<<<<<<< HEAD
-=======
 import javax.persistence.PersistenceException;
 
 
->>>>>>> Catch persistence exceptions on user registration. A proper handling with known errors being properly presented to the user is still required
 public class Application extends Controller {
 
     public static Result login() {
@@ -47,9 +40,8 @@ public class Application extends Controller {
     	return ok(register.render(form(User.class)));
     }
 
-    //TODO: lots of boilerplate. Refactor!
     public static Result registerAction(){
-    	Form<User> userForm = form(User.class).bindFromRequest();
+        Form<User> userForm = form(User.class).bindFromRequest();
         if (userForm.hasErrors()) {
             return badRequest(register.render(userForm));
         } else {
@@ -78,54 +70,4 @@ public class Application extends Controller {
             }
         }
     }
-
-	public static Result login() {
-		return ok(login.render(form(Login.class)));
-	}
-
-	public static Result logout() {
-		session().clear();
-		flash("success", "You've been logged out");
-		return redirect(routes.Application.login());
-	}
-
-	public static Result authenticate() {
-
-		Form<Login> loginForm = form(Login.class).bindFromRequest();
-		if (loginForm.hasErrors()) {
-			return badRequest(login.render(loginForm));
-		} else {
-			session("email", loginForm.get().email);
-			return redirect(routes.Dashboard.index());
-		}
-	}
-
-	public static Result registerForward() {
-		return ok(register.render(form(User.class)));
-	}
-
-	public static Result registerAction() {
-		Form<User> userForm = form(User.class).bindFromRequest();
-		// userForm.get().password = "1234567899876543211234567";
-		// userForm.get().save();
-		// TODO: Necessario enviar o email para o email registado pelo
-		// utilizador, se falhar o envio, retornar o form em erro
-		if (userForm.hasErrors()) {
-			return badRequest(register.render(userForm));
-		} else {
-			InvalidValue invalid = Ebean.validate(userForm.get());
-			if (!invalid.isError()) {
-				Ebean.save(userForm.get());
-				flash("success",
-						"Pedido registado. Assim que a sua conta estiver autorizada, receberá um email em "
-								+ userForm.get().email
-								+ " com as instruções para finalização do registo.");
-				return redirect(routes.Application.login());
-			}
-			//TODO: PT?
-			userForm.reject(invalid.getMessage());
-			return badRequest(register.render(userForm));
-		}
-	}
-
 }
