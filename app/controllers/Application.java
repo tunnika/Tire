a53105467698784e9933ff06c9b534c3e196213e
Joaquim Rego <kimile@gmail.com>
2012-04-23 00:20:56 +0100

    Merge registerAction() including Ebean property validations

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,7 +2,10 @@ package controllers;
 
 import com.avaje.ebean.Ebean;
 import com.avaje.ebean.InvalidValue;
+<<<<<<< HEAD
 
+=======
+>>>>>>> Merge registerAction() including Ebean property validations
 import models.*;
 import play.data.Form;
 import play.data.validation.ValidationError;
@@ -43,24 +46,36 @@ public class Application extends Controller {
     public static Result registerForward(){
     	return ok(register.render(form(User.class)));
     }
-    
+
+    //TODO: lots of boilerplate. Refactor!
     public static Result registerAction(){
     	Form<User> userForm = form(User.class).bindFromRequest();
         if (userForm.hasErrors()) {
             return badRequest(register.render(userForm));
         } else {
-            User u = userForm.get();
-            try{
-                u.save();
-            }catch(PersistenceException pe){
-                //TODO: how to handle persistence exceptions properly?
-                // Found error when filling in a duplicate email
-                ValidationError ve = new ValidationError("", pe.getMessage(), null);
-                userForm.reject(ve);
+            // make form complies with all the validations at the "database" level
+            InvalidValue invalid = Ebean.validate(userForm.get());
+            if(!invalid.isError()){ // database constraints are valid
+                try{
+                    userForm.get().save();
+                    flash("success", Messages.get("registration.pending.approval", userForm.get().email));
+                    // TODO: send notification to the user by email. If email fails, return form with error
+                    return redirect(routes.Application.login());
+                } catch (PersistenceException pe){
+                    // unfortunately, we still need to catch
+                    // exceptions when writing to database
+                    //TODO: how to handle persistence exceptions properly?
+                    // Found error when filling in a duplicate email
+                    ValidationError ve = new ValidationError("", pe.getMessage(), null);
+                    userForm.reject(ve);
+                    return badRequest(register.render(userForm));
+                }
+            } else {
+                // failed database validations
+                //TODO: PT?
+                userForm.reject(invalid.getMessage());
                 return badRequest(register.render(userForm));
             }
-        	flash("success", Messages.get("registration.pending.approval", u.email));
-        	return redirect(routes.Application.login());
         }
     }
 
