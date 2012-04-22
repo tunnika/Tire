fa1d40b1eefadada184b59a9a9b8d862bb0b0fb4
Carlos Martins <martins.carlos@gmail.com>
2012-04-22 21:45:23 +0100

    css classes added to beautify forms

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,5 +1,8 @@
 package controllers;
 
+import com.avaje.ebean.Ebean;
+import com.avaje.ebean.InvalidValue;
+
 import models.*;
 import play.data.Form;
 import play.i18n.Messages;
@@ -7,9 +10,9 @@ import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.*;
 
-
 public class Application extends Controller {
 
+<<<<<<< HEAD
     public static Result login() {
         return ok(login.render(form(Login.class)));
     }
@@ -46,4 +49,54 @@ public class Application extends Controller {
         	return redirect(routes.Application.login());
         }
     }
+=======
+	public static Result login() {
+		return ok(login.render(form(Login.class)));
+	}
+
+	public static Result logout() {
+		session().clear();
+		flash("success", "You've been logged out");
+		return redirect(routes.Application.login());
+	}
+
+	public static Result authenticate() {
+
+		Form<Login> loginForm = form(Login.class).bindFromRequest();
+		if (loginForm.hasErrors()) {
+			return badRequest(login.render(loginForm));
+		} else {
+			session("email", loginForm.get().email);
+			return redirect(routes.Dashboard.index());
+		}
+	}
+
+	public static Result registerForward() {
+		return ok(register.render(form(User.class)));
+	}
+
+	public static Result registerAction() {
+		Form<User> userForm = form(User.class).bindFromRequest();
+		// userForm.get().password = "1234567899876543211234567";
+		// userForm.get().save();
+		// TODO: Necessario enviar o email para o email registado pelo
+		// utilizador, se falhar o envio, retornar o form em erro
+		if (userForm.hasErrors()) {
+			return badRequest(register.render(userForm));
+		} else {
+			InvalidValue invalid = Ebean.validate(userForm.get());
+			if (!invalid.isError()) {
+				Ebean.save(userForm.get());
+				flash("success",
+						"Pedido registado. Assim que a sua conta estiver autorizada, receberá um email em "
+								+ userForm.get().email
+								+ " com as instruções para finalização do registo.");
+				return redirect(routes.Application.login());
+			}
+			//TODO: PT?
+			userForm.reject(invalid.getMessage());
+			return badRequest(register.render(userForm));
+		}
+	}
+>>>>>>> css classes added to beautify forms
 }
