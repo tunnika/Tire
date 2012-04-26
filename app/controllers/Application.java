e2fa7283a018260a87ec468e05bcb69892493aa6
Joaquim Rego <kimile@gmail.com>
2012-04-26 15:14:47 +0100

    Refector of registerAction() and change extend to EnhancedController

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,19 +1,20 @@
 package controllers;
 
-import com.avaje.ebean.Ebean;
-import com.avaje.ebean.InvalidValue;
-import models.*;
+import exceptions.FormValidationException;
+import models.Login;
+import models.User;
 import play.data.Form;
-import play.data.validation.ValidationError;
 import play.i18n.Messages;
-import play.mvc.Controller;
 import play.mvc.Result;
-import views.html.*;
+import util.EnhancedController;
+import views.html.about;
+import views.html.login;
+import views.html.register;
 
 import javax.persistence.PersistenceException;
 
 
-public class Application extends Controller {
+public class Application extends EnhancedController {
 
     public static Result login() {
         return ok(login.render(form(Login.class)));
@@ -40,37 +41,23 @@ public class Application extends Controller {
     	return ok(register.render(form(User.class)));
     }
 
-    //TODO: lots of boilerplate. Refactor!
     public static Result registerAction(){
         Form<User> userForm = form(User.class).bindFromRequest();
-        if (userForm.hasErrors()) {
+        try {
+            handleSaveForm(userForm);
+        } catch (FormValidationException e) {
             return badRequest(register.render(userForm));
-        } else {
-            // make form complies with all the validations at the "database" level
-            InvalidValue invalid = Ebean.validate(userForm.get());
-            if(invalid==null){ // database constraints are valid
-                try{
-                    userForm.get().save();
-                    flash("success", Messages.get("registration.pending.approval", userForm.get().email));
-                    // TODO: send notification to the user by email. If email fails, return form with error
-                    return redirect(routes.Application.login());
-                } catch (PersistenceException pe){
-                    // unfortunately, we still need to catch
-                    // exceptions when writing to database
-                    //TODO: how to handle persistence exceptions properly?
-                    // Found error when filling in a duplicate email
-                    ValidationError ve = new ValidationError("", pe.getMessage(), null);
-                    userForm.reject(ve);
-                    return badRequest(register.render(userForm));
-                }
-            } else {
-                // failed database validations
-                //TODO: PT?
-                //TODO: is getMessage() the way to go? it looks like it never gets populated
-                userForm.reject(invalid.getMessage());
-                return badRequest(register.render(userForm));
-            }
+        } catch (PersistenceException pe){
+            // add global erro to response
+            // override system message DUPLICATE_PK with registration.duplicate.email
+            addPersistenceError(userForm, pe.getCause(), err(
+                    "DUPLICATE_PK", Messages.get("registration.duplicate.email", userForm.get().email)));
+            return badRequest(register.render(userForm));
         }
+
+        flash("success", Messages.get("registration.pending.approval", userForm.get().email));
+        // TODO: send notification to the user by email. If email fails, return form with error
+        return redirect(routes.Application.login());
     }
     
     public static Result aboutForward(){
